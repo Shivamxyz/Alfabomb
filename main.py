007e8cb2b2246ec3137a366d1d269e9a6221c47c
Shivam Rajput <114960692+Shivamxyz@users.noreply.github.com>
2024-01-22 23:02:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def start(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Hello! This is your Telegram bot.')
 
 def main() -> None:
-    updater = Updater(TELEGRAM_BOT_TOKEN)
+    updater = Updater(6803163694:AAEQX-EYOnvjuJwNe4qwFTGUqSXJycdt0h8)
 
     dp = updater.dispatcher
 
